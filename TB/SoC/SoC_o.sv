c734f6024893f1c858ef62d876c3fcc891e7bdec
zoubr99 <Zoubr99@gmail.com>
2024-03-05 01:03:53 +0000

    fixing a Decoder Problem

--- TB/SoC/SoC_o.sv
+++ TB/SoC/SoC_o.sv
@@ -1,185 +1,168 @@
-/**
- * Step 5: Creating a RISC-V processor
- *         The register bank and the state machine
- * LEDs show state.
- * DONE*
- */
-
-//`default_nettype none
 `include "clk_divider.sv"
+//`default_nettype none
 
-module SOC (
-    input  CLK,        // system clock 
-    input  RESET,      // reset button
-    output [4:0] LEDS, // system LEDs
-    input  RXD,        // UART receive
-    output TXD         // UART transmit
+module SOC( // declaring the inputs and outputs
+    input logic CLK,
+    input logic RESET,
+    output logic [4:0] LEDS,
+    input logic RXD, // UART receiver
+    output logic TXD // UART Transmitter
 );
 
-   wire clk;    // internal clock
-   wire resetn; // internal reset signal, goes low on reset
-   
-   reg [31:0] MEM [0:255]; 
-   reg [31:0] PC;          // program counter
-   reg [31:0] instr;       // current instruction
+logic clk;
+logic resetn;
 
-   initial begin
-      PC = 0;
-      
-      // add x0, x0, x0
-      //                   rs2   rs1  add  rd   ALUREG
-      instr = 32'b0000000_00000_00000_000_00000_0110011;
-      // add x1, x0, x0
-      //                    rs2   rs1  add  rd   ALUREG
-      MEM[0] = 32'b0000000_00000_00000_000_00001_0110011;
-      // addi x1, x1, 1
-      //             imm         rs1  add  rd   ALUIMM
-      MEM[1] = 32'b000000000001_00001_000_00001_0010011;
-      // addi x1, x1, 1
-      //             imm         rs1  add  rd   ALUIMM
-      MEM[2] = 32'b000000000001_00001_000_00001_0010011;
-      // addi x1, x1, 1
-      //             imm         rs1  add  rd   ALUIMM
-      MEM[3] = 32'b000000000001_00001_000_00001_0010011;
-      // addi x1, x1, 1
-      //             imm         rs1  add  rd   ALUIMM
-      MEM[4] = 32'b000000000001_00001_000_00001_0010011;
 
-      // ebreak
-      //                                        SYSTEM
-      MEM[5] = 32'b000000000001_00000_000_00000_1110011;
-       
+// declaring a 5 bit register
+reg [31:0] MEM [0:255]; // BRAM
+reg [31:0] PC = 0; // Program Counter
+
+        // add x0, x0, x0
+              //                   rs2   rs1  add  rd   ALUREG
+reg [31:0] C_INST =  32'b0000000_00000_00000_000_00000_0110011; //cureent instruction reg
+
+  // initiliasing the SoC Memmory with some instructions
+   initial begin // this is only for TB simulation purposes
+        // add x1, x0, x0
+        //                    rs2   rs1  add  rd  ALUREG
+        MEM[0] = 32'b0000000_00000_00000_000_00001_0110011;
+        // addi x1, x1, 1
+        //             imm         rs1  add  rd   ALUIMM
+        MEM[1] = 32'b000000000001_00001_000_00001_0010011;
+        // addi x1, x1, 1
+        //             imm         rs1  add  rd   ALUIMM
+        MEM[2] = 32'b000000000001_00001_000_00001_0010011;
+        // addi x1, x1, 1
+        //             imm         rs1  add  rd   ALUIMM
+        MEM[3] = 32'b000000000001_00001_000_00001_0010011;
+        // addi x1, x1, 1
+        //             imm         rs1  add  rd   ALUIMM
+        MEM[4] = 32'b000000000001_00001_000_00001_0010011;
+        // lw x2,0(x1)
+        //             imm         rs1   w   rd   LOAD
+        MEM[5] = 32'b000000000000_00001_010_00010_0000011;
+        // sw x2,0(x1)
+        //             imm   rs2   rs1   w   imm  STORE
+        MEM[6] = 32'b000000_00010_00001_010_00000_0100011;
+        
+        // ebreak
+        //                                        SYSTEM
+        MEM[7] = 32'b000000000001_00000_000_00000_1110011;
+        
    end
 
-   
-   // See the table P. 105 in RISC-V manual
-   
-   // The 10 RISC-V instructions
-   wire isALUreg  =  (instr[6:0] == 7'b0110011); // rd <- rs1 OP rs2   
-   wire isALUimm  =  (instr[6:0] == 7'b0010011); // rd <- rs1 OP Iimm
-   wire isBranch  =  (instr[6:0] == 7'b1100011); // if(rs1 OP rs2) PC<-PC+Bimm
-   wire isJALR    =  (instr[6:0] == 7'b1100111); // rd <- PC+4; PC<-rs1+Iimm
-   wire isJAL     =  (instr[6:0] == 7'b1101111); // rd <- PC+4; PC<-PC+Jimm
-   wire isAUIPC   =  (instr[6:0] == 7'b0010111); // rd <- PC + Uimm
-   wire isLUI     =  (instr[6:0] == 7'b0110111); // rd <- Uimm   
-   wire isLoad    =  (instr[6:0] == 7'b0000011); // rd <- mem[rs1+Iimm]
-   wire isStore   =  (instr[6:0] == 7'b0100011); // mem[rs1+Simm] <- rs2
-   wire isSYSTEM  =  (instr[6:0] == 7'b1110011); // special
 
-   // The 5 immediate formats
-   wire [31:0] Uimm={    instr[31],   instr[30:12], {12{1'b0}}};
-   wire [31:0] Iimm={{21{instr[31]}}, instr[30:20]};
-   wire [31:0] Simm={{21{instr[31]}}, instr[30:25],instr[11:7]};
-   wire [31:0] Bimm={{20{instr[31]}}, instr[7],instr[30:25],instr[11:8],1'b0};
-   wire [31:0] Jimm={{12{instr[31]}}, instr[19:12],instr[20],instr[30:21],1'b0};
+   // RISCV instructions Decoder
+  //*********************************************************************************//
+  //*********************************************************************************//
 
-   // Source and destination registers
-   wire [4:0] rs1Id = instr[19:15];
-   wire [4:0] rs2Id = instr[24:20];
-   wire [4:0] rdId  = instr[11:7];
+  // The 10 RISC-V instructions
+   wire isALUreg  =  (C_INST[6:0] == 7'b0110011); // rd <- rs1 OP rs2   
+   wire isALUimm  =  (C_INST[6:0] == 7'b0010011); // rd <- rs1 OP Iimm
+   wire isBranch  =  (C_INST[6:0] == 7'b1100011); // if(rs1 OP rs2) PC<-PC+Bimm
+   wire isJALR    =  (C_INST[6:0] == 7'b1100111); // rd <- PC+4; PC<-rs1+Iimm
+   wire isJAL     =  (C_INST[6:0] == 7'b1101111); // rd <- PC+4; PC<-PC+Jimm
+   wire isAUIPC   =  (C_INST[6:0] == 7'b0010111); // rd <- PC + Uimm
+   wire isLUI     =  (C_INST[6:0] == 7'b0110111); // rd <- Uimm   
+   wire isLoad    =  (C_INST[6:0] == 7'b0000011); // rd <- mem[rs1+Iimm]
+   wire isStore   =  (C_INST[6:0] == 7'b0100011); // mem[rs1+Simm] <- rs2
+   wire isSYSTEM  =  (C_INST[6:0] == 7'b1110011); // special
 
-   // function codes
-   wire [2:0] funct3 = instr[14:12];
-   wire [6:0] funct7 = instr[31:25];
-   
-   // The registers bank
-   reg [31:0] RegisterBank [0:31];
-   reg [31:0] rs1;
-   reg [31:0] rs2;
-   wire [31:0] writeBackData;
-   wire        writeBackEn;
-   assign writeBackData = 0; // for now
-   assign writeBackEn = 0;   // for now
+  // The 5 immediate formats
+   wire [31:0] Uimm={    C_INST[31],   C_INST[30:12], {12{1'b0}}};
+   wire [31:0] Iimm={{21{C_INST[31]}}, C_INST[30:20]};
+   wire [31:0] Simm={{21{C_INST[31]}}, C_INST[30:25],C_INST[11:7]};
+   wire [31:0] Bimm={{20{C_INST[31]}}, C_INST[7],C_INST[30:25],C_INST[11:8],1'b0};
+   wire [31:0] Jimm={{12{C_INST[31]}}, C_INST[19:12],C_INST[20],C_INST[30:21],1'b0};
 
-`ifdef BENCH   
-   integer i;
-   initial begin
-      for(i=0; i<32; ++i) begin
-	 RegisterBank[i] = 0;
+  // Source and destination registers
+   wire [4:0] rs1Id = C_INST[19:15]; // source reg 1 
+   wire [4:0] rs2Id = C_INST[24:20]; // source reg 2
+   wire [4:0] rdId  = C_INST[11:7]; // destenation reg
+
+  // function codes
+   wire [2:0] funct3 = C_INST[14:12]; // function to be performed on src 1 and 2 regs
+   wire [6:0] funct7 = C_INST[31:25];
+
+  //*********************************************************************************//
+  //*********************************************************************************//
+
+
+  //*********************************************************************************//
+  //*********************************************************************************//
+  // Registers File
+  reg [31:0] RegisterFile [0:31];
+  reg [31:0] rs1;
+  reg [31:0] rs2;
+  wire [31:0] writeBackData;
+  wire        writeBackEn;
+
+  assign writeBackData = 0; // for now
+  assign writeBackEn = 0; // for now
+  //*********************************************************************************//
+  //*********************************************************************************//
+
+  localparam FETCH_INSTR = 0;
+  localparam FETCH_REGS = 1;
+  localparam EXECUTE = 2;
+
+  reg [1:0] state = FETCH_INSTR;
+
+
+  // sequential logic based on clock edges
+  always_ff @(posedge clk, negedge resetn) begin
+      if (!resetn) begin 
+        PC <= 0;
+        state <= FETCH_INSTR;
+        C_INST <= 32'b0000000_00000_00000_000_00000_0110011; // NOP
       end
-   end
-`endif   
+      else begin
+        if (writeBackEn && rdId != 0) begin
+          RegisterFile[rdId] <= writeBackData;
+        end
 
-   // The state machine
-   
-   localparam FETCH_INSTR = 0;
-   localparam FETCH_REGS  = 1;
-   localparam EXECUTE     = 2;
-   reg [1:0] state = FETCH_INSTR;
-   
-   always @(posedge clk) begin
-      if(!resetn) begin
-	 PC    <= 0;
-	 state <= FETCH_INSTR;
-	 instr <= 32'b0000000_00000_00000_000_00000_0110011; // NOP
-      end else begin
-	 if(writeBackEn && rdId != 0) begin
-	    RegisterBank[rdId] <= writeBackData;
-	 end
-	 
-	 case(state)
-	   FETCH_INSTR: begin
-	      instr <= MEM[PC];
-	      state <= FETCH_REGS;
-	   end
-	   FETCH_REGS: begin
-	      rs1 <= RegisterBank[rs1Id];
-	      rs2 <= RegisterBank[rs2Id];
-	      state <= EXECUTE;
-	   end
-	   EXECUTE: begin
-	      if(!isSYSTEM) begin
-		 PC <= PC + 1;
-	      end
-	      state <= FETCH_INSTR;	      
-`ifdef BENCH      
-	      if(isSYSTEM) $finish();
-`endif      
-	   end
-	 endcase
-      end 
-   end 
+              case(state)
 
-   assign LEDS = isSYSTEM ? 31 : (1 << state);
+              FETCH_INSTR: begin
+                C_INST <= MEM[PC]; // assign the instruction in which the program counter is currently pointing towards
+                state <=  FETCH_REGS;
+              end
 
-`ifdef BENCH
-   always @(posedge clk) begin
-      if(state == FETCH_REGS) begin
-	 case (1'b1)
-	   isALUreg: $display(
-			      "ALUreg rd=%d rs1=%d rs2=%d funct3=%b",
-			      rdId, rs1Id, rs2Id, funct3
-			      );
-	   isALUimm: $display(
-			      "ALUimm rd=%d rs1=%d imm=%0d funct3=%b",
-			      rdId, rs1Id, Iimm, funct3
-			      );
-	   isBranch: $display("BRANCH");
-	   isJAL:    $display("JAL");
-	   isJALR:   $display("JALR");
-	   isAUIPC:  $display("AUIPC");
-	   isLUI:    $display("LUI");	
-	   isLoad:   $display("LOAD");
-	   isStore:  $display("STORE");
-	   isSYSTEM: $display("SYSTEM");
-	 endcase 
-	 if(isSYSTEM) begin
-	    $finish();
-	 end
-      end 
-   end
-`endif	      
+              FETCH_REGS: begin
+                rs1 <= RegisterFile[rs1Id];
+                rs2 <= RegisterFile[rs2Id];
+                state <= EXECUTE;
+              end
 
-   // Gearbox and reset circuitry.
-   clk_divider #(
-     .SLOW(26) // Divide clock frequency by 2^21
-   )CW(
+              EXECUTE: begin
+                if(!isSYSTEM) begin
+                PC <= PC + 1; // increase the program counter ie go to the next instruction in RAM
+                end
+                else if(isSYSTEM) begin
+                PC <= 0;
+                C_INST <= 32'b0000000_00000_00000_000_00000_0110011; // NOP
+                end
+                state <= FETCH_INSTR;
+              end
+            
+              endcase
+      end
+  end
+
+  clk_divider #(.SLOW(27))
+  clk_divider (
+
      .CLK(CLK),
      .RESET(RESET),
      .clk(clk),
      .resetn(resetn)
-   );
-   
-   assign TXD  = 1'b0; // not used for now   
-   
+
+    );
+
+
+
+  // assigning the count to the leds
+  assign LEDS = (isSYSTEM) ? 16 : {PC[0],isALUimm,isStore,isLoad,isALUreg};
+  assign TXD = 1'b0;
+    
 endmodule
